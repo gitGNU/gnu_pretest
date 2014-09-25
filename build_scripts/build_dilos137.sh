f2671b8f5bbcd4e491e724f09cd6a085ab79d8bb
A. Gordon <assafgordon@gmail.com>
2014-09-25 18:03:48 -0400

    scripts: DilOS 1.3.7 build script

---
+++ build_scripts/build_dilos137.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# Copyright (C) 2014 Assaf Gordon (assafgordon@gmail.com)
+#
+# This file is part of PreTest
+#
+# PreTest is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PreTest is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PreTest If not, see <http://www.gnu.org/licenses/>.
+
+NAME=dilos137
+ISO_URL=http://apt2.dilos.org/dilos/isos/dilos-core_install-14-09-19_17-23-1.3.7.18.iso
+ISO_FILE=$(basename "$ISO_URL")
+INSECURE_DOWNLOAD=yes
+QCOW2_SIZE=5G
+RAM=1024
+QCOW2_FILE="$NAME.qcow2"
+
+die() { BASE=$(basename "$0") ; echo "$BASE: error: $@" >&2 ; exit 1 ; }
+
+## Create QCOW2 Image file
+test -e "$QCOW2_FILE" \
+    && die "qcow2 image file '$QCOW2_FILE' already exists. aborting"
+qemu-img create -q -f qcow2 "$QCOW2_FILE" "$QCOW2_SIZE" \
+        || die "failed to create qcow2 file '$QCOW2_FILE'"
+
+## Download ISO (if not already downloaded)
+if ! test -e "$ISO_FILE" ; then
+    test "x$INSECURE_DOWNLOAD" = "xyes" \
+        && INSECURE_PARAM=--no-check-certificate
+    wget --quiet $INSECURE_PARAM -O "$ISO_FILE" "$ISO_URL" \
+        || die "failed to download '$ISO_URL'"
+fi
+
+## Run KVM and install Dilos 1.3.7.18
+##
+## NOTE:
+##   1. DilOS installer works with "-curses"
+##   2. DilOS requires "-machine pc-1.1", otherwise boot hangs forever.
+kvm -name "$NAME" \
+    -m "$RAM" \
+    -net nic,model=virtio \
+    -net user \
+    -boot cd \
+    -drive file="$QCOW2_FILE",if=virtio,media=disk,index=0 \
+    -cdrom "$ISO_FILE" \
+    -redir tcp:2222::22 \
+    -machine pc-1.1 \
+    -curses
+
+## vim: set shiftwidth=4:
+## vim: set tabstop=4:
+## vim: set expandtab:
