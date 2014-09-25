de683fc65cce3a518fae1189e6f9b2c1d8f3d40c
A. Gordon <assafgordon@gmail.com>
2014-09-25 18:02:32 -0400

    maint: add GPLv3+ to shell scripts

--- build_scripts/build_freebsd10.sh
+++ build_scripts/build_freebsd10.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# Copyright (C) 2014 Assaf Gordon (assafgordon@gmail.com)
+#
+# This file is part of PreTest
+#
+# PreTest is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PreTest is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PreTest If not, see <http://www.gnu.org/licenses/>.
+
 NAME=freebsd10
 ISO_URL=ftp://ftp.freebsd.org/pub/FreeBSD/releases/amd64/amd64/ISO-IMAGES/10.0/FreeBSD-10.0-RELEASE-amd64-disc1.iso
 ISO_FILE=$(basename "$ISO_URL")
