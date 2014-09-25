4921462be064f4ee42adfea85e821435fcebe2cc
A. Gordon <assafgordon@gmail.com>
2014-09-25 17:54:05 -0400

    get-versions script: improved reporting

--- run_scripts/get_versions_raw.sh
+++ run_scripts/get_versions_raw.sh
@@ -1,18 +1,60 @@
-printf AUTOCONF-VER: ; autoconf --version | head -n1 ;
-printf AUTOMAKE-VER: ; automake --version | head -n1 ;
-printf AUTOPOINT-VER: ; autopoint --version | head -n1 ;
-if make --version 1>/dev/null 2>/dev/null ; then
-  printf MAKE-VER: ; make --version | head -n1 ;
-else
-  echo MAKE-VER: non-gnu make
-fi
-printf GIT-VER: ; git --version | head -n1 ;
-printf WGET-VER: ; wget --version | head -n1 ;
-printf MAKEINFO-VER: ; makeinfo --version | head -n1 ;
-printf CC-VER: ; cc --version | head -n1 ;
-printf UNAME-S: ; uname -s || echo ;
-printf UNAME-R: ; uname -r || echo ;
-printf UNAME-M: ; uname -m || echo ;
-printf UNAME-p: ; uname -p || echo ;
-printf UNAME-i: ; uname -i || echo ;
-printf UNAME-o: ; uname -o || echo ;
+# Print versions of install programs and system information
+#
+####
+# Copyright (C) 2014 Assaf Gordon (assafgordon@gmail.com)
+#
+# This file is part of PreTest
+#
+# PreTest is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PreTest is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PreTest If not, see <http://www.gnu.org/licenses/>.
+####
+# Usage example:
+#   When a VM is running (and have port 22 redirected to port 2222),
+#   use this script like so:
+#     ssh -p 2222 miles@localhost < misc_scripts/get_versions.sh
+#
+for PROG in \
+autoconf \
+automake \
+autopoint \
+autoreconf \
+make \
+gmake \
+makeinfo \
+git \
+wget \
+rsync \
+gcc \
+cc \
+;
+do
+  printf "${PROG}-version: " ;
+  if which ${PROG} 1>/dev/null 2>/dev/null ; then
+    if $PROG --version 1>/dev/null 2>/dev/null ; then
+      $PROG --version | head -n1 ;
+    else
+      echo no-version
+    fi ;
+  else
+    echo missing
+  fi ;
+done ;
+
+for FLAG in -s -r -m -p -i -o -v ; do
+  printf "uname$FLAG: " ;
+  if uname $FLAG 2>/dev/null 1>/dev/null ; then
+    uname $FLAG
+  else
+    echo
+  fi ;
+done ;
